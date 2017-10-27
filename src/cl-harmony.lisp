1b4f43f2a26792353699ee045255e4eaa3fa3129
MegaLoler/Madeline/Aardbei <megaloler9000@gmail.com>
2017-10-27 04:14:41 -0500

    op code receive skeleton

--- src/cl-harmony.lisp
+++ src/cl-harmony.lisp
@@ -1,3 +1,6 @@
+(ql:quickload :dexador)
+(ql:quickload :websocket-driver-client)
+(ql:quickload :jonathan)
 
 (defparameter gateway-url-suffix "/?v=6&encoding=json")
 
@@ -69,6 +72,82 @@
           (lambda (message)
 	    (on-recv (jonathan:parse message)))))
 
-;; receive message from websock
+;; on receive websock messages by opcode
+;; opcode 0
+(defun on-recv-dispatch (msg)
+  nil)
+
+;; opcode 1
+(defun on-recv-heartbeat (msg)
+  nil)
+
+;; opcode 2
+(defun on-recv-identify (msg)
+  nil)
+
+;; opcode 3
+(defun on-recv-status-update (msg)
+  nil)
+
+;; opcode 4
+(defun on-recv-voice-status-update (msg)
+  nil)
+
+;; opcode 5
+(defun on-recv-voice-server-ping (msg)
+  nil)
+
+;; opcode 6
+(defun on-recv-resume (msg)
+  nil)
+
+;; opcode 7
+(defun on-recv-reconnect (msg)
+  nil)
+
+;; opcode 8
+(defun on-recv-request-guild-members (msg)
+  nil)
+
+;; opcode 9
+(defun on-recv-invalid-session (msg)
+  nil)
+
+;; opcode 10
+(defun on-recv-hello (msg)
+  nil)
+
+;; opcode 11
+(defun on-recv-heartbeat-ack (msg)
+  nil)
+
+;; receive message from websock and dispatch to handler
 (defun on-recv (msg)
-  (print msg))
+  (let ((op (getf msg :|op|)))
+    (cond ((= op 0)
+	   (on-recv-dispatch msg))
+	  ((= op 1)
+	   (on-recv-heartbeat msg))
+	  ((= op 2)
+	   (on-recv-identify msg))
+	  ((= op 3)
+	   (on-recv-status-update msg))
+	  ((= op 4)
+	   (on-recv-voice-status-update msg))
+	  ((= op 5)
+	   (on-recv-voice-server-ping msg))
+	  ((= op 6)
+	   (on-recv-resume msg))
+	  ((= op 7)
+	   (on-recv-reconnect msg))
+	  ((= op 8)
+	   (on-recv-request-guild-members msg))
+	  ((= op 9)
+	   (on-recv-invalid-session msg))
+	  ((= op 10)
+	   (on-recv-hello msg))
+	  ((= op 11)
+	   (on-recv-heartbeat-ack msg))
+	  (T
+	    ;; not sure if this should be an error to the user or not?
+	    (error "Received invalid opcode!")))))
