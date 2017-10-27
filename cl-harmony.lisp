46751dbc7c8f30de29cf6f9122851d460577ea34
MegaLoler/Madeline/Aardbei <megaloler9000@gmail.com>
2017-10-27 00:07:34 -0500

    okay now the token is removed for real

--- cl-harmony.lisp
+++ cl-harmony.lisp
@@ -1,5 +1,5 @@
 (ql:quickload "dexador")
 
 (dex:post "https://discordapp.com/api/v6/"
-          :headers '(("Authorization" . "Bot MzY2Mjk4NDEyNDQzODkzNzYx.DNSRNA.T0iYDzRgD4Na_LubrYtVjLrOx3Y")
+          :headers '(("Authorization" . "Bot ___")
 		     ("User-Agent" . "DiscordBot ($url, $versionNumber)")))
