6423ae34f46fbad98a890f3d13ec3a1f29954ea4
MegaLoler/Madeline/Aardbei <megaloler9000@gmail.com>
2017-10-27 01:27:30 -0500

    content length

--- src/cl-harmony.lisp
+++ src/cl-harmony.lisp
@@ -1,13 +1,13 @@
 (ql:quickload "dexador")
 
-
 (defun str-concat (&rest strings)
   (apply #'concatenate 'string strings))
 
 (defun post (endpoint token)
   (dex:post (str-concat "https://discordapp.com/api/v6/" endpoint)
 	    :headers '(("Authorization" . (str-concat "Bot " token))
-		       ("User-Agent" . "DiscordBot ('N/A', '0.0.1')"))))
+		       ("User-Agent" . "DiscordBot ('n/a', '0.0.1')")
+		       ("Content-length" . "0"))))
 
 ;; doesn't require token ?
 (defun gateway ()
