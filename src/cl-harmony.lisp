65089f3f63a8f5f18f43e3c7f48610e3d19abc36
MegaLoler/Madeline/Aardbei <megaloler9000@gmail.com>
2017-10-27 01:16:23 -0500

    trying to gateway

--- src/cl-harmony.lisp
+++ src/cl-harmony.lisp
@@ -7,4 +7,8 @@
 (defun post (endpoint token)
   (dex:post (str-concat "https://discordapp.com/api/v6/" endpoint)
 	    :headers '(("Authorization" . (str-concat "Bot " token))
-		       ("User-Agent" . "DiscordBot (N/A, 0.0.1)"))))
+		       ("User-Agent" . "DiscordBot ('N/A', '0.0.1')"))))
+
+;; doesn't require token ?
+(defun gateway ()
+  (post "gateway" ""))
