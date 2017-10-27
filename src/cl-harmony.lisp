0d6b4454d0c9f4b7fd0cb6256a14663a6f226176
MegaLoler/Madeline/Aardbei <megaloler9000@gmail.com>
2017-10-27 01:52:43 -0500

    restructure; get

--- src/cl-harmony.lisp
+++ src/cl-harmony.lisp
@@ -3,11 +3,13 @@
 ;; should these be constants?
 (defvar bot-url "N/A")
 (defvar bot-ver-str "0.0.1")
+(defvar discord-api-ver "v6")
+(defvar discord-api-base-url "https://discordapp.com/api")
 
 (defun str-concat (&rest strings)
   (apply #'concatenate 'string strings))
 
-;; unsure if the url and version string should be quoted or not
+;; unsure if the url and version string should be quoted or not in the header
 (defun mk-user-agent-str ()
   (str-concat "DiscordBot ('" bot-url "', '" bot-ver-str "')"))
 
@@ -16,10 +18,16 @@
         (cons "User-Agent" (mk-user-agent-str))
         (cons "Content-length" "0")))
 
-(defun post (endpoint token)
-  (dex:post (str-concat "https://discordapp.com/api/v6/" endpoint)
+(defun mk-api-url (endpoint)
+  (str-concat discord-api-base-url "/" discord-api-ver "/" endpoint))
+
+;; is 'get' reserved?
+(defun get-rq (endpoint)
+  (dex:get (mk-api-url endpoint)))
+
+(defun post-rq (endpoint token)
+  (dex:post (mk-api-url endpoint)
 	    :headers (mk-headers token)))
 
-;; doesn't require token ?
 (defun gateway ()
-  (post "gateway" ""))
+  (get-rq "gateway"))
