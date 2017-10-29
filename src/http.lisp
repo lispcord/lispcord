78bdd7db80336277b338f8fb9a18545ae16d319f
spreadLink <siboru@googlemail.com>
2017-10-29 11:48:15 +0100

    slight cleanup

--- src/http.lisp
+++ src/http.lisp
@@ -3,6 +3,5 @@
 ; send a message!
 (defun send (bot channel-id content)
   (post-rq (str-concat "channels/" channel-id "/messages") bot
-	   ;(jmake (list (cons "content" content)))))
-	   (list (cons "content" content))))
+	   (alist "content" content)))
 
